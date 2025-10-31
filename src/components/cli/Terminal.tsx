a4f3d826cc2c5e5bbe757d7bae53531aaa5a649e
zxcoding123 <ahmadaquino.2002@gmail.com>
2025-10-31 21:19:24 +0800

    Update Terminal.tsx

--- src/components/cli/Terminal.tsx
+++ src/components/cli/Terminal.tsx
@@ -80,12 +80,17 @@ export function Terminal() {
             }
         }
     };
+    
     // Initial welcome message
     useEffect(() => {
         setHistory([<Home key="home-initial" />]);
     }, []);
 
-
+    useEffect(() => {
+        if (inputRef.current) {
+          inputRef.current.focus();
+        }
+    }, []);
 
     useEffect(() => {
         if (scrollRef.current) {
@@ -96,7 +101,7 @@ export function Terminal() {
     return (
         <div 
             className="w-full h-[90vh] max-w-5xl bg-background rounded-lg border-2 border-border shadow-2xl backdrop-blur-sm font-code text-base"
-        
+            onClick={() => inputRef.current?.focus()}
         >
             <div className="bg-secondary p-3 flex items-center gap-2 border-b-2 border-primary rounded-t-md">
                 <div className="flex gap-2">
